a767458bed901a26101ea4ff545cf31c2e4258e3
yizhu0103 <as23063368@gmail.com>
2024-08-22 13:00:46 +0800

    0822

--- js/sreach.js
+++ js/sreach.js
@@ -50,6 +50,39 @@
 
 }); */
 
+var VisibleMenu = ''; // 記錄目前顯示的子選單的 ID
+
+// 顯示或隱藏子選單
+function switchMenu( theMainMenu, theSubMenu, theEvent ){
+    var SubMenu = document.getElementById( theSubMenu );
+    if( SubMenu.style.display == 'none' ){ // 顯示子選單
+        SubMenu.style.minWidth = theMainMenu.clientWidth; // 讓子選單的最小寬度與主選單相同 (僅為了美觀)
+        SubMenu.style.display = 'block';
+        hideMenu(); // 隱藏子選單
+        VisibleMenu = theSubMenu;
+    }
+    else{ // 隱藏子選單
+        if( theEvent != 'MouseOver' || VisibleMenu != theSubMenu ){
+            SubMenu.style.display = 'none';
+            VisibleMenu = '';
+        }
+    }
+}
+
+// 隱藏子選單
+function hideMenu(){
+    if( VisibleMenu != '' ){
+        document.getElementById( VisibleMenu ).style.display = 'none';
+    }
+    VisibleMenu = '';
+}
+
+
+
+
+
+
+
 function inv() {
     var invantory = document.getElementById("invantory");
     var courseList = document.getElementById("courseList");
